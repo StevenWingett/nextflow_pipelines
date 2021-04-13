59997c194c001bb363644dbbb5377beb208f4360
StevenWingett <steven.wingett@mrc-lmb.cam.ac.uk>
2021-04-13 10:32:43 +0100

    Updated script to automatically download genomes etc.

--- helper_scripts/download_genomes.sh
+++ helper_scripts/download_genomes.sh
@@ -2,53 +2,89 @@
 
 # BASH script to download genomes of interest into the current working
 # directory from iGenomes:
-# https://emea.support.illumina.com/sequencing/sequencing_software/igenome.html
-# A website tool was used to assist with the downloading:
-# https://ewels.github.io/AWS-iGenomes/
-#
-# This script requires AWS to be installed
 #
 # The script also needs to be run on a version of the Linux in which the command 
 # "rename" takes regular expression.  Some versions of Linux "rename" do not do this.
 
 # Make folder
-mkdir Nextflow_iGenomes
-cd Nextflow_iGenomes/
+mkdir Genome_References
+cd Genome_References/
+
+####################
+####################
+#####################
+# Ensembl references
+#####################
+#####################
+####################
+mkdir Ensembl
+cd Ensembl/
+
 
 ####################
 #####################
 # HUMAN
 #####################
 #####################
+mkdir Homo_sapiens
+cd Homo_sapiens/
 
 #####################
 # GRCh38
 #####################
+mkdir GRCh38
+cd GRCh38
 
 # FASTA
 ################
-aws s3 --no-sign-request --region eu-west-1 sync s3://ngi-igenomes/igenomes/Homo_sapiens/NCBI/GRCh38/Sequence/Chromosomes/ ./references/Homo_sapiens/NCBI/GRCh38/Sequence/Chromosomes/
-
-#Move non-standard chromosomes
-cd references/Homo_sapiens/NCBI/GRCh38/Sequence/Chromosomes
-rename s/^chr/Homo_sapiens_GRCh38_chr/ chr*
-mkdir extra_sequences
-mv *.fa extra_sequences/
-mv extra_sequences/*chr[a-z].* .
-mv extra_sequences/Homo_sapiens_GRCh38_chr[0-9].* .
-mv extra_sequences/Homo_sapiens_GRCh38_chr[0-9][0-9].* .
-cd -
+mkdir FASTA
+cd FASTA/
+mkdir chromosomes
+cd chromosomes
+lftp -e "mget Homo_sapiens.GRCh38.dna.chromosome.*.fa.gz; bye" http://ftp.ensembl.org/pub/release-102/fasta/homo_sapiens/dna/
+gunzip *.gz
 
 # Bowtie2
-###############
-# Needs to be made de novo, since iGenomes Bowtie2 index include non-regular
-# sequences
-mkdir references/Homo_sapiens/NCBI/GRCh38/Sequence/Chromosomes/Bowtie2Index
+################
+genome=$(ls -m *.fa)
+genome=$(echo $genome | sed 's/ //g')
+bowtie2-build $genome Homo_sapiens.GRCh38.dna.chromosome > bowtie2-build.out
+mkdir ../../Bowtie2
+mv *.bt2  ../../Bowtie2/
+mv bowtie2-build.out  ../../Bowtie2/
+
+# HISAT2
+################
+hisat2-build $genome Homo_sapiens.GRCh38.dna.chromosome > hisat2-build.out
+mkdir ../../HISAT2
+mv *.ht2  ../../HISAT2/
+mv hisat2-build.out  ../../HISAT2/
+cd ../..
 
 # GTF
-###############
-aws s3 --no-sign-request --region eu-west-1 sync s3://ngi-igenomes/igenomes/Homo_sapiens/NCBI/GRCh38/Annotation/Genes/ ./references/Homo_sapiens/NCBI/GRCh38/Annotation/Genes/ --exclude "*" --include "genes.gtf"
-mv references/Homo_sapiens/NCBI/GRCh38/Annotation/Genes/genes.gtf references/Homo_sapiens/NCBI/GRCh38/Annotation/Genes/Homo_sapiens_GRCh38_genes.gtf
+#################
+mkdir GTF
+cd GTF/
+wget -nv http://ftp.ensembl.org/pub/release-102/gtf/homo_sapiens/Homo_sapiens.GRCh38.102.chr.gtf.gz
+gunzip *.gz
+gtf=$(ls *.gtf)
+splice_sites=$(echo $gtf | sed 's/\.gtf$/.ss/g')
+hisat2_extract_splice_sites.py $gtf > $splice_sites
+exon=$(echo $gtf | sed 's/\.gtf$/.exon/g')
+hisat2_extract_exons.py $gtf > $exon
+cd ..
+
+# Make NextFlow genome text
+###########################
+echo -e "name\tGRCh38" > GRCh38.genome
+echo -e "species\tHomo sapiens" >> GRCh38.genome
+echo -e "fasta\t$PWD/FASTA/chromosomes/" >> GRCh38.genome
+echo -e "bowtie2\t$PWD/Bowtie2/Homo_sapiens.GRCh38.dna.chromosome" >> GRCh38.genome
+echo -e "gtf\t$PWD/GTF/Homo_sapiens.GRCh38.102.chr.gtf" >> GRCh38.genome
+echo -e "hisat2_splices\t$PWD/GTF/Homo_sapiens.GRCh38.102.chr.ss" >> GRCh38.genome
+echo -e "hisat2\t$PWD/HISAT2/Homo_sapiens.GRCh38.dna.chromosome" >> GRCh38.genome
+echo -e "\n" >> GRCh38.genome
+cd ..
 
 
 ####################
@@ -56,43 +92,68 @@ mv references/Homo_sapiens/NCBI/GRCh38/Annotation/Genes/genes.gtf references/Hom
 # Mouse
 #####################
 #####################
+mkdir Mus_musculus
+cd Mus_musculus/
 
 #####################
 # GRCm38
 #####################
+mkdir GRCm38
+cd GRCm38
 
 # FASTA
 ################
-aws s3 --no-sign-request --region eu-west-1 sync s3://ngi-igenomes/igenomes/Mus_musculus/NCBI/GRCm38/Sequence/Chromosomes/ ./references/Mus_musculus/NCBI/GRCm38/Sequence/Chromosomes/
-cd references/Mus_musculus/NCBI/GRCm38/Sequence/Chromosomes/
-rename -e s/^/Mus_musculus_GRCm38_chr_/ *.fa
-cd -
+mkdir FASTA
+cd FASTA/
+mkdir chromosomes
+cd chromosomes
+
+lftp -e "mget Mus_musculus.GRCm38.dna.chromosome.*.fa.gz; bye" http://ftp.ensembl.org/pub/release-100/fasta/mus_musculus/dna/
+gunzip *.gz
 
 # Bowtie2
-###############
-aws s3 --no-sign-request --region eu-west-1 sync s3://ngi-igenomes/igenomes/Mus_musculus/NCBI/GRCm38/Sequence/Bowtie2Index/ ./references/Mus_musculus/NCBI/GRCm38/Sequence/Bowtie2Index/
-cd references/Mus_musculus/NCBI/GRCm38/Sequence/Bowtie2Index/
-rename -e s/genome/Mus_musculus_GRCm38/ *
-cd -
+################
+genome=$(ls -m *.fa)
+genome=$(echo $genome | sed 's/ //g')
+bowtie2-build $genome Mus_musculus.GRCm38.dna.chromosome > bowtie2-build.out
+mkdir ../../Bowtie2
+mv *.bt2  ../../Bowtie2/
+mv bowtie2-build.out  ../../Bowtie2/
+
+# HISAT2
+################
+hisat2-build $genome Mus_musculus.GRCm38.dna.chromosome > hisat2-build.out
+mkdir ../../HISAT2
+mv *.ht2  ../../HISAT2/
+mv hisat2-build.out  ../../HISAT2/
+cd ../..
 
 # GTF
-###############
-aws s3 --no-sign-request --region eu-west-1 sync s3://ngi-igenomes/igenomes/Mus_musculus/NCBI/GRCm38/Annotation/Genes/ ./references/Mus_musculus/NCBI/GRCm38/Annotation/Genes/ --exclude "*" --include "genes.gtf"
-mv references/Mus_musculus/NCBI/GRCm38/Annotation/Genes/genes.gtf references/Mus_musculus/NCBI/GRCm38/Annotation/Genes/Mus_musculus_GRCm38.gtf
+#################
+mkdir GTF
+cd GTF/
+wget -nv http://ftp.ensembl.org/pub/release-100/gtf/mus_musculus/Mus_musculus.GRCm38.100.chr.gtf.gz
+gunzip *.gz
+gtf=$(ls *.gtf)
+splice_sites=$(echo $gtf | sed 's/\.gtf$/.ss/g')
+hisat2_extract_splice_sites.py $gtf > $splice_sites
+exon=$(echo $gtf | sed 's/\.gtf$/.exon/g')
+hisat2_extract_exons.py $gtf > $exon
+cd ..
+
+# Make NextFlow genome text
+###########################
+echo -e "name\tGRCm38" > GRCm38.genome
+echo -e "species\tMus musculus" >> GRCm38.genome
+echo -e "fasta\t$PWD/FASTA/chromosomes/" >> GRCm38.genome
+echo -e "bowtie2\t$PWD/Bowtie2/Mus_musculus.GRCh38.dna.chromosome" >> GRCm38.genome
+echo -e "gtf\t$PWD/GTF/Mus_musculus.GRCm38.100.chr.gtf" >> GRCm38.genome
+echo -e "hisat2_splices\t$PWD/GTF/Mus_musculus.GRCm38.100.chr.ss" >> GRCm38.genome
+echo -e "hisat2\t$PWD/HISAT2/Mus_musculus.GRCh38.dna.chromosome" >> GRCm38.genome
+echo -e "\n" >> GRCm38.genome
+cd ..
 
 
-####################
-#####################
-# Macaque
-#####################
-#####################
-
-#####################
-# GRCm38
-#####################
-
-# FASTA
-################
 
 
 
